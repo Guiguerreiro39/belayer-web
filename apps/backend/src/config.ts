e1d6f3e597cb324d892a9b9f4607d125d462a9b2
Guilherme Guerreiro <mail@guilhermegr.com>
2023-01-02 22:53:26 +0000

    remove backend

--- apps/backend/src/config.ts
+++
@@ -1,9 +0,0 @@
-export const config = () => ({
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: parseInt(process.env.PORT) || 3000,
-  FRONTEND_URL: process.env.FRONTEND_URL,
-  JWT: {
-    SECRET: process.env.JWT_SECRET,
-    TIMEOUT: process.env.JWT_TIMEOUT,
-  },
-});
